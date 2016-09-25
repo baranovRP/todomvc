cd55d0ea088220eea7caffdb5e5e1159b2dcb518
Roman Baranov <baranov.r.p@gmail.com>
2016-09-25 09:46:37 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* eslint no-use-before-define: ["error", { "functions": false }] */
+/* eslint no-global-assign: "warn" */
+/* eslint-env browser */
+
+import func from './file';
+
+const document = window.document;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  devtool: 'source-map',
+  entry: ['babel-polyfill', './js/main.js'],
+  output: {
+    path: 'dist/js',
+    filename: '[name].js',
+  },
+  watch: true,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+};
